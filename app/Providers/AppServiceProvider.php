a7c39a8e42bc7d6cb36aeddde47543a580d96db5
Michelangelo <piga.michelangelo@gmail.com>
2022-09-02 17:09:51 +0200

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
-use Nette\Schema\Schema as SchemaSchema;
+
 
 class AppServiceProvider extends ServiceProvider
 {
